b11bbd545c2de863b2e7226281dcaf2c82592214
Matt Chowning <mchowning@gmail.com>
2017-05-06 11:24:26 -0400

    Basic haskell setup for 99 bottles kata

---
+++ 99Bottles/haskell/app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import NinetyNineBottles
+
+main :: IO ()
+main = sing
---
+++ 99Bottles/haskell/src/NinetyNineBottles.hs
@@ -0,0 +1,13 @@
+module NinetyNineBottles where
+
+sing :: IO ()
+sing = undefined
+
+song :: [String]
+song = undefined
+
+verses :: Int -> Int -> [String]
+verses = undefined
+
+verse :: Int -> String
+verse = undefined
