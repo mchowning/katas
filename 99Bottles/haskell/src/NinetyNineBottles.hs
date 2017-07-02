025650071924932c0420148d0687acfa549e2d09
Matt Chowning <mchowning@gmail.com>
2017-07-01 20:03:47 -0400

    Adding haskell and kotlin

--- 99Bottles/haskell/src/NinetyNineBottles.hs
+++ 99Bottles/haskell/src/NinetyNineBottles.hs
@@ -1,13 +1,37 @@
-module NinetyNineBottles where
+module NinetyNineBottles ( sing
+                         , song
+                         , verses
+                         , verse
+                         )where
+
+import Text.Printf
+import Data.Char (toUpper)
 
 sing :: IO ()
-sing = undefined
+sing = putStr $ unlines song
 
 song :: [String]
-song = undefined
+song = verses 99 0
 
 verses :: Int -> Int -> [String]
-verses = undefined
+verses start end = map verse $ enumFromThenTo start (start-1) end
 
 verse :: Int -> String
-verse = undefined
+verse n = let stringFormat = "%s of beer on the wall, %s of beer.\n\
+                             \%s, %s of beer on the wall."
+              currentBottles@(c:cs) = bottles n
+              currentBottlesCap = toUpper c : cs
+              nextBottles = bottles (n-1)
+          in printf stringFormat currentBottlesCap currentBottles (takeDown n) nextBottles
+
+
+bottles :: Int -> String
+bottles (-1) = "99 bottles"
+bottles 0 = "no more bottles"
+bottles 1 = "1 bottle"
+bottles n = show n ++ " bottles"
+
+takeDown :: Int -> String
+takeDown 0 = "Go to the store and buy some more"
+takeDown 1 = "Take it down and pass it around"
+takeDown _ = "Take one down and pass it around"
--- 99Bottles/haskell/test/Spec.hs
+++ 99Bottles/haskell/test/Spec.hs
@@ -331,4 +331,3 @@ main = hspec $ do
 
         , "No more bottles of beer on the wall, no more bottles of beer.\n\
             \Go to the store and buy some more, 99 bottles of beer on the wall." ]
-
